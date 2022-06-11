60289c4adbc05d17440897f18e30976e0cbf2aba
vishal1106 <101567849+vishal1106@users.noreply.github.com>
2022-06-11 21:13:26 +0530

    updated TO-DOS

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,10 +9,12 @@ const Todo = () => {
   return (
     <div>
         <h2>todo list </h2>
-    <input type="text" placeholder='Write Something' onChange={(e)=>{
+    <input style={{width:"270px"}}type="text" placeholder='Write Something' onChange={(e)=>{
      
      setTodos(e.target.value)
+    
     }}/>
+    
     <button onClick={()=>{
     
     
@@ -23,8 +25,13 @@ const Todo = () => {
        }
      ])
     }}>Add</button>
+     {console.log(todo)}
    {  todo.map((tod) =>(
-    <div>{tod.value}</div>
+    <div style={{display:"flex",justifyContent:"space-between",boxShadow: "rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px",width:"300px",margin:"auto" ,marginTop:"20px" ,textAlign:"left",paddingLeft:"10px" ,height:"40px"}}>
+      <h3 style={{marginTop:"2px"}}>{tod.value}</h3>
+      <input style={{marginRight:"10px",marginTop:"10px"}} type="radio" checked/>
+    </div>
+
     ))}
     </div>
   );
